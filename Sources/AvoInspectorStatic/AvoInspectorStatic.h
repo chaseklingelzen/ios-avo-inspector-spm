7e602419abc894eaf3826d89b73175b6d40f8b5d
Alex Verein <tpom6oh@gmail.com>
2021-12-08 23:37:00 +0300

    Place modulemap under sources

---
+++ Sources/AvoInspectorStatic/AvoInspectorStatic.h
@@ -0,0 +1,9 @@
+//
+//  AvoInspectorStatic.h
+//  AvoInspectorStatic
+//
+//  Created by Alex Verein on 08.12.2021.
+//
+
+#import <AvoInspectorStatic/AvoInspector.h>
+#import <AvoInspectorStatic/Inspector.h>
