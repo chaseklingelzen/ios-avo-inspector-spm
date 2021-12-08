b6e8314f49f5364d77f951ed757d5ef3aa1a225a
Alex Verein <tpom6oh@gmail.com>
2021-12-08 15:55:29 +0300

    Initial Commit

---
+++ AvoInspectorStatic/AvoInspectorStatic.h
@@ -0,0 +1,12 @@
+//
+//  AvoInspectorStatic.h
+//  AvoInspectorStatic
+//
+//  Created by Alex Verein on 08.12.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AvoInspectorStatic : NSObject
+
+@end
---
+++ AvoInspectorStatic/AvoInspectorStatic.m
@@ -0,0 +1,12 @@
+//
+//  AvoInspectorStatic.m
+//  AvoInspectorStatic
+//
+//  Created by Alex Verein on 08.12.2021.
+//
+
+#import "AvoInspectorStatic.h"
+
+@implementation AvoInspectorStatic
+
+@end
